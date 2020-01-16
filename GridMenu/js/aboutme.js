570019c17ff8728136c3e02caaef7563d6a10e61
black45 <33381841+black45@users.noreply.github.com>
2020-01-16 20:50:01 +0000

    Add files via upload

---
+++ GridMenu/js/aboutme.js
@@ -0,0 +1,11 @@
+
+$(function() {
+    $("#aboutMe").text(
+"Hello everyone, my name is Nikita, if I tell you briefly about myself,\n" 
++"	my hobby is programming, because I like to create something interesting and new "
++"	and surprise people with this. I also like to work on all kinds of projects that send me."
++"	\n My main programming language is Java but "
++"	I also can write both Javascript and PHP so I will be happy to help you with your projects"
+
+    	);
+});
